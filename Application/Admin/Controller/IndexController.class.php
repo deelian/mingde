a67df2112b0b93f8b097bdfca37ca45d8d9c8b03
deelian <master@paituola.cn>
2017-12-22 01:15:36 +0800

    loginPage&initializeFunc

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -5,10 +5,14 @@ use Think\Controller;
 
 class IndexController extends Controller
 {
+
+    function _initialize()
+    {
+        echo "string";
+    }
+
     public function index()
     {
-        echo U();
-        echo u('asdf/asdf/fasdf/asdf');
-        p('abc');
+        $this->display();
     }
 }

54e43e43a790f3661649a98d7c4e915b760ffa6a
www.xiadao.ml <master@paituola.cn>
2017-12-23 16:15:47 +0800

    errorPageSet

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,6 +6,8 @@ return array(
 	'URL_CASE_INSENSITIVE'  =>  false,
 
 //	'TMPL_EXCEPTION_FILE'	=>	'./Public/404.tpl',
+//	'ERROR_PAGE'     		=>	'./Public/404.tpl',
+
 
 	'URL_ROUTER_ON'   		=>  true,
 	'URL_HTML_SUFFIX'       =>  'Power_by_deelian',  // URL伪静态后缀设置

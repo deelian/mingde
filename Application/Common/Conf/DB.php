e4d7a441437f1045eb9cd27aa8487081489e1354
www.xiadao.ml <master@paituola.cn>
2017-12-27 15:59:51 +0800

    config

--- Application/Common/Conf/DB.php
+++ Application/Common/Conf/DB.php
@@ -2,11 +2,11 @@
 return array(
 
 	'DB_TYPE'               =>  'MYSQL',     // 数据库类型
-	'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-	'DB_NAME'               =>  '',          // 数据库名
+	'DB_HOST'               =>  '139.159.230.61', // 服务器地址
+	'DB_NAME'               =>  'userInfo',          // 数据库名
 	'DB_USER'               =>  'root',      // 用户名
-	'DB_PWD'                =>  'root',          // 密码
+	'DB_PWD'                =>  'masterOnline',          // 密码
 	'DB_PORT'               =>  '3306',        // 端口
-	'DB_PREFIX'             =>  '',    // 数据库表前缀
+	'DB_PREFIX'             =>  'wd_',    // 数据库表前缀
 );
  ?>

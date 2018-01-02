c19af4b4be6c31cd5298030867915970c657b94a
www.xiadao.ml <master@paituola.cn>
2018-01-02 17:35:44 +0800

    epd

--- Application/Admin/Controller/IndexController.class.php
+++ Application/Admin/Controller/IndexController.class.php
@@ -23,7 +23,16 @@ class IndexController extends Controller
 
     public function getUserInfo(){
         $user   = M('user');
-        $res    = $user->where('user_id <= 2 ')->getField('user_id,name,real_name,sex,mobile');
+        $condition      = array(
+            'name'   => array(
+                'like',
+                '%小%'
+            )
+        );
+//        p($condition,1);
+        $res    = $user->where($condition)->order('mobile asc')->getField('user_id,name,real_name,sex,mobile');
+//        p($res);
+        p($user->getLastSql());
         foreach($res as $resK => $resV){
             switch ($resV['sex']) {
                 case '0':
@@ -53,9 +62,9 @@ class IndexController extends Controller
 //        import("Org.Util.PHPExcel");
         Vendor('Excel.PHPExcel');
 //        import("Org.Util.PHPExcel.Writer.Excel5");
-        Vendor('Excel.PHPExcel.Writer.Excel5');
+//        Vendor('Excel.PHPExcel.Writer.Excel5');
 //        import("Org.Util.PHPExcel.IOFactory.php");
-        Vendor('Excel.PHPExcel.IOFactory');
+//        Vendor('Excel.PHPExcel.IOFactory');
 
 //        $date = date("Y_m_d",time());
         $date = time();
@@ -66,7 +75,7 @@ class IndexController extends Controller
         $objProps = $objPHPExcel->getProperties();
 
         //设置表头
-        $key = ord("C");
+        $key = ord("A");
         //print_r($headArr);exit;
         foreach($headArr as $v){
             $colum = chr($key);
@@ -103,68 +112,6 @@ class IndexController extends Controller
         $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
         $objWriter->save('php://output'); //文件通过浏览器下载
         exit;
-    }
-
-    public function dataExport($data)
-    {
-
-//        p($objPHPExcel);
-//        return $objPHPExcel->createSheet($data);
-//        p($objExcel);
-//        $cellName = array('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA','AB','AC','AD','AE','AF','AG','AH','AI','AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ');
-//
-//        $objPHPExcel->getActiveSheet(0)->mergeCells('A1:'.$cellName[$cellNum-1].'1');//合并单元格
-//        // $objPHPExcel->setActiveSheetIndex(0)->setCellValue('A1', $expTitle.'  Export time:'.date('Y-m-d H:i:s'));
-//        for($i=0;$i<$cellNum;$i++){
-//            $objPHPExcel->setActiveSheetIndex(0)->setCellValue($cellName[$i].'2', $expCellName[$i][1]);
-//        }
-//        // Miscellaneous glyphs, UTF-8
-//        for($i=0;$i<$dataNum;$i++){
-//            for($j=0;$j<$cellNum;$j++){
-//                $objPHPExcel->getActiveSheet(0)->setCellValue($cellName[$j].($i+3), $expTableData[$i][$expCellName[$j][0]]);
-//            }
-//        }
-//
-//        header('pragma:public');
-//        header('Content-type:application/vnd.ms-excel;charset=utf-8;name="'.$xlsTitle.'.xls"');
-//        header("Content-Disposition:attachment;filename=$fileName.xls");//attachment新窗口打印inline本窗口打印
-//        $objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel5');
-//        $objWriter->save('php://output');
-//        exit;
-    }
-    /**
-     *
-     * 导出Excel
-     */
-    function expUser(){//导出Excel
-        $xlsName  = "User";
-        $xlsCell  = array(
-            array('id','账号序列'),
-            array('truename','名字'),
-            array('sex','性别'),
-            array('res_id','院系'),
-            array('sp_id','专业'),
-            array('class','班级'),
-            array('year','毕业时间'),
-            array('city','所在地'),
-            array('company','单位'),
-            array('zhicheng','职称'),
-            array('zhiwu','职务'),
-            array('jibie','级别'),
-            array('tel','电话'),
-            array('qq','qq'),
-            array('email','邮箱'),
-            array('honor','荣誉'),
-            array('remark','备注')
-        );
-        $xlsModel = M('Member');
-
-        $xlsData  = $xlsModel->Field('id,truename,sex,res_id,sp_id,class,year,city,company,zhicheng,zhiwu,jibie,tel,qq,email,honor,remark')->select();
-        foreach ($xlsData as $k => $v)
-        {
-            $xlsData[$k]['sex']=$v['sex']==1?'男':'女';
-        }
-        $this->exportExcel($xlsName,$xlsCell,$xlsData);
     }
 
 
